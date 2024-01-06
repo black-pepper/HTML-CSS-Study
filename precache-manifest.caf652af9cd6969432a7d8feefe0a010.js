98c8ae78544108898816cdcc402f8caf1bbf164b
Uju <56465463+black-pepper@users.noreply.github.com>
2024-01-06 22:54:02 +0900

    Updates

---
+++ precache-manifest.caf652af9cd6969432a7d8feefe0a010.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34130f2c9a0fe5f75cf36424a6b639e1",
+    "url": "/HTML-CSS-Study/index.html"
+  },
+  {
+    "revision": "f30af6c6c0891d9b2c36",
+    "url": "/HTML-CSS-Study/static/css/2.cc82f8aa.chunk.css"
+  },
+  {
+    "revision": "fc5a10c0ddb3b18ce834",
+    "url": "/HTML-CSS-Study/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "f30af6c6c0891d9b2c36",
+    "url": "/HTML-CSS-Study/static/js/2.c4ce0c77.chunk.js"
+  },
+  {
+    "revision": "5086b69a25eb35d4c984844f4a3e129c",
+    "url": "/HTML-CSS-Study/static/js/2.c4ce0c77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a13f0315eda6d0fb8fc",
+    "url": "/HTML-CSS-Study/static/js/3.60274f7f.chunk.js"
+  },
+  {
+    "revision": "fc5a10c0ddb3b18ce834",
+    "url": "/HTML-CSS-Study/static/js/main.7b618915.chunk.js"
+  },
+  {
+    "revision": "f93b4d41f3f0677ce4cb",
+    "url": "/HTML-CSS-Study/static/js/runtime-main.ca433744.js"
+  }
+]);
