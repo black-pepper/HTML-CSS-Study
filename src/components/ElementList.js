ffce5a52884672c3fadb20f1d56344845b3deb42
Uju <56465463+black-pepper@users.noreply.github.com>
2024-01-08 21:11:37 +0900

    Update ElementList

--- src/components/ElementList.js
+++ src/components/ElementList.js
@@ -1,35 +1,46 @@
 import React, { useState } from 'react';
-import { Modal, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 import SelectTagList from './SelectTagList';
 
 const ElementList = () => {
+
+    const [elements, setElements] = useState([]);
+
+    const handleTagSelect = (tagType) => {
+        setElements([...elements, tagType]);
+    };
+
+    const handleTagRemove = (indexToRemove) => {
+        const updatedElements = elements.filter((_, index) => index !== indexToRemove);
+        setElements(updatedElements);
+    };
+
     return (
         <div class="list-group" style={{margin:10}}>
-            <li class="list-group-item">요소 목록1</li>
-            <li class="list-group-item">요소 목록2</li>
-            <li class="list-group-item">요소 목록3</li>
+            {elements.map((element, index) => (
+                <li class="list-group-item d-flex justify-content-between align-items-center" key={index}>
+                    {element}
+                        <button  type="button" class="btn-close" aria-label="Close" onClick={() => handleTagRemove(index)}></button>
+                </li>
+            ))}
             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Launch demo modal
+            Add Tag
             </button>
 
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Select Tag</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Select Tag</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <SelectTagList onTagSelect={handleTagSelect} />
+                        </div>
+                    </div>
                 </div>
-                <SelectTagList/>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-                </div>
-            </div>
             </div>
         </div>
     )
--- src/components/SelectTagList.js
+++ src/components/SelectTagList.js
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
 import'bootstrap/dist/css/bootstrap.min.css';
 
-const SelectTagList = () => {
+const SelectTagList = ({ onTagSelect }) => {
+  const handleTagClick = (tagType) => {
+    // 클릭한 태그를 부모 컴포넌트에 전달
+    onTagSelect(tagType);
+  };
+
     return (
       <div className="list-group" style={{ margin: 10 }}>
         {TagType.map((tagType, index) => (
-          <li className="list-group-item" key={index}>
+          <button 
+            className="list-group-item list-group-item-action" 
+            key={index}
+            onClick={() => handleTagClick(tagType)}
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          >
             {tagType}
-          </li>
+          </button>
         ))}
       </div>
     );
