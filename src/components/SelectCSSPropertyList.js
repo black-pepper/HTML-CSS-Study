a14b6da1c25ab4569c42170cdfa1925b9a07d244
Uju <56465463+black-pepper@users.noreply.github.com>
2024-02-13 23:51:39 +0900

    Add select property

---
+++ src/components/SelectCSSPropertyList.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import'bootstrap/dist/css/bootstrap.min.css';
+
+const SelectCSSProperty = ({ onTagSelect }) => {
+  const handleTagClick = (tagType) => {
+    // 클릭한 태그를 부모 컴포넌트에 전달
+    onTagSelect(tagType);
+  };
+
+    return (
+      <div className="list-group">
+        {TagType.map((tagType, index) => (
+          <button 
+            className="list-group-item list-group-item-action" 
+            key={index}
+            onClick={() => handleTagClick(tagType)}
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          >
+            {tagType}
+          </button>
+        ))}
+      </div>
+    );
+  };
+export default SelectCSSProperty;
+
+const TagType = [
+    "div", "section", "article", "nav", "span", "p", "a", "br"
+]
---
+++ src/components/SelectHTMLProperty.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import'bootstrap/dist/css/bootstrap.min.css';
+
+const SelectHTMLProperty = ({ onTagSelect }) => {
+  const handleTagClick = (tagType) => {
+    // 클릭한 태그를 부모 컴포넌트에 전달
+    onTagSelect(tagType);
+  };
+
+    return (
+      <div className="list-group">
+        {TagType.map((tagType, index) => (
+          <button 
+            className="list-group-item list-group-item-action" 
+            key={index}
+            onClick={() => handleTagClick(tagType)}
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          >
+            {tagType}
+          </button>
+        ))}
+      </div>
+    );
+  };
+export default SelectHTMLProperty;
+
+const TagType = [
+    "div", "section", "article", "nav", "span", "p", "a", "br"
+]
