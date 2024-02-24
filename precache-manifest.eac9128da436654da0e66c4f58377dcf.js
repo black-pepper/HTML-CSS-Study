2979ace0a41853f3ea24e39007988b7a71ade09d
Uju <56465463+black-pepper@users.noreply.github.com>
2024-02-24 23:06:50 +0900

    Updates

--- precache-manifest.eac9128da436654da0e66c4f58377dcf.js
+++ precache-manifest.eac9128da436654da0e66c4f58377dcf.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1282f0e09c809b48ec625e9133dd6643",
+    "revision": "38d7e192d6b1afa15ca2394b6f48cd38",
     "url": "/HTML-CSS-Study/index.html"
   },
   {
-    "revision": "889b0ff150d5f7dfa5e5",
+    "revision": "f83006f1c6a7d0794887",
     "url": "/HTML-CSS-Study/static/css/2.cc82f8aa.chunk.css"
   },
   {
-    "revision": "036986874cae46e03998",
+    "revision": "f4cb6a58f6a6654249e3",
     "url": "/HTML-CSS-Study/static/css/main.435d5fe5.chunk.css"
   },
   {
-    "revision": "889b0ff150d5f7dfa5e5",
-    "url": "/HTML-CSS-Study/static/js/2.445c0d72.chunk.js"
+    "revision": "f83006f1c6a7d0794887",
+    "url": "/HTML-CSS-Study/static/js/2.4c37f99d.chunk.js"
   },
   {
     "revision": "a530a386256823463a2653e8f4a10bf3",
-    "url": "/HTML-CSS-Study/static/js/2.445c0d72.chunk.js.LICENSE.txt"
+    "url": "/HTML-CSS-Study/static/js/2.4c37f99d.chunk.js.LICENSE.txt"
   },
   {
     "revision": "2c0df7b366e2e3a9f42d",
     "url": "/HTML-CSS-Study/static/js/3.b1c15188.chunk.js"
   },
   {
-    "revision": "036986874cae46e03998",
-    "url": "/HTML-CSS-Study/static/js/main.77ecfd23.chunk.js"
+    "revision": "f4cb6a58f6a6654249e3",
+    "url": "/HTML-CSS-Study/static/js/main.a14ab776.chunk.js"
   },
   {
     "revision": "6fc4258eb6aec35c2702",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HTML-CSS-Study/precache-manifest.519c53b24ec4ba130036b3c01325a437.js"
+  "/HTML-CSS-Study/precache-manifest.eac9128da436654da0e66c4f58377dcf.js"
 );
 
 self.addEventListener('message', (event) => {
