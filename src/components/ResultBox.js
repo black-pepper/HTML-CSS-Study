3cec46725f21a45d6eff99e645901b29e1f066cd
Uju <56465463+black-pepper@users.noreply.github.com>
2024-04-05 21:26:19 +0900

    Update Result Box

--- src/components/ResultBox.js
+++ src/components/ResultBox.js
@@ -16,29 +16,29 @@ const ResultBox = ({ tagData }) => {
       result += `<${tag}`;
       // HTML 속성 추가
       Object.entries(htmlProperty).forEach(([key, value]) => {
-        if(key!="content") result += ` ${key}="${value.value}"`;
+        if (key != "content") result += ` ${key}="${value.value}"`;
       });
       // CSS 속성 추가
-      if(!(Object.keys(cssProperty).length===0)) { 
+      if (!(Object.keys(cssProperty).length === 0)) {
         result += ` style="`;
         Object.entries(cssProperty).forEach(([key, value]) => {
-          if(value) result += `${key}:${value.value};`;
+          if (value) result += `${key}:${value.value};`;
         });
-        result += `">${(htmlProperty.content)?htmlProperty.content.value:''}`;
-          if (tagData.current[target] && tagData.current[target].children) {
-            for (const child of tagData.current[target].children) DFS(child);
-          }
+        result += `">${(htmlProperty.content) ? htmlProperty.content.value : ''}`;
+        if (tagData.current[target] && tagData.current[target].children) {
+          for (const child of tagData.current[target].children) DFS(child);
+        }
         result += `</${tag}>`; // 태그 닫기
       } else {
-        result += `>${(htmlProperty.content)?htmlProperty.content.value:''}`
-          if (tagData.current[target] && tagData.current[target].children) {
-            for (const child of tagData.current[target].children) DFS(child);
-          }
+        result += `>${(htmlProperty.content) ? htmlProperty.content.value : ''}`
+        if (tagData.current[target] && tagData.current[target].children) {
+          for (const child of tagData.current[target].children) DFS(child);
+        }
         result += `</${tag}>`; // 태그 닫기
       }
     }
-    for(const i in tagData.current) {
-      if(tagData.current[i].depth == 0) DFS(i);
+    for (const i in tagData.current) {
+      if (tagData.current[i].depth == 0) DFS(i);
     }
     return result;
   };
@@ -55,50 +55,79 @@ const ResultBox = ({ tagData }) => {
       const cssProperty = tagData.current[target].cssProperty;
       const depth = tagData.current[target].depth;
       // 각 태그와 속성에 맞게 HTML 코드 추가
-      if(depth>0){
+      if (depth > 0) {
         result += "  ".repeat(depth);
       }
       result += `<${tag}`;
       // HTML 속성 추가
       Object.entries(htmlProperty).forEach(([key, value]) => {
-        if(key!="content") result += ` ${key}="${value.value}"`;
+        if (key != "content") result += ` ${key}="${value.value}"`;
       });
       // CSS 속성 추가
-      if(!(Object.keys(cssProperty).length===0)) { 
+      if (!(Object.keys(cssProperty).length === 0)) {
         result += ` style="`;
         Object.entries(cssProperty).forEach(([key, value]) => {
-          if(value) result += `${key}:${value.value};`;
+          if (value) result += `${key}:${value.value};`;
         });
-        result += `">\n${(htmlProperty.content)?"  ".repeat(depth+1)+htmlProperty.content.value+'\n':''}`;
-          if (tagData.current[target] && tagData.current[target].children) {
-            for (const child of tagData.current[target].children) DFS(child);
-          }
-        if(depth>0){
+        result += `">\n${(htmlProperty.content) ? "  ".repeat(depth + 1) + htmlProperty.content.value + '\n' : ''}`;
+        if (tagData.current[target] && tagData.current[target].children) {
+          for (const child of tagData.current[target].children) DFS(child);
+        }
+        if (depth > 0) {
           result += "  ".repeat(depth);
         }
         result += `</${tag}>\n`; // 태그 닫기
       } else {
-        result += `>\n${(htmlProperty.content)?"  ".repeat(depth+1)+htmlProperty.content.value+'\n':''}`
-          if (tagData.current[target] && tagData.current[target].children) {
-            for (const child of tagData.current[target].children) DFS(child);
-          }
-        if(depth>0){
+        result += `>\n${(htmlProperty.content) ? "  ".repeat(depth + 1) + htmlProperty.content.value + '\n' : ''}`
+        if (tagData.current[target] && tagData.current[target].children) {
+          for (const child of tagData.current[target].children) DFS(child);
+        }
+        if (depth > 0) {
           result += "  ".repeat(depth);
         }
         result += `</${tag}>\n`; // 태그 닫기
       }
     }
-    for(const i in tagData.current) {
-      if(tagData.current[i].depth == 0) DFS(i);
+    for (const i in tagData.current) {
+      if (tagData.current[i].depth == 0) DFS(i);
     }
     return result;
   }
 
+  const openResultWindow = () => {
+    // 결과를 보여줄 HTML 코드를 작성합니다.
+    const resultHTML = getResult();
+
+    // 새 창을 엽니다.
+    const resultWindow = window.open("", "_blank");
+
+    // 새 창에 HTML을 쓰고 닫힘 이벤트를 처리합니다.
+    resultWindow.document.write(resultHTML);
+    resultWindow.document.close();
+  };
+
+
   return (
-    <div className="card" style={{ marginTop: 10, minHeight: "150px"}}>
-      <div className="card-body" dangerouslySetInnerHTML={{ __html: getResult() }} />
-      <div className="card-footer text-body-secondary" style={{ minHeight: "40px", whiteSpace:"pre"}}>
-        {getTagResult()}
+    <div className="card mb-3" style={{ marginTop: 10 }}>
+      <div className="row g-0">
+        <div className="col"
+          style={{ padding: 10 }}
+          dangerouslySetInnerHTML={{ __html: getResult() }}
+        />
+        <div className="col text-bg-light"
+          style={{ minHeight: "40px", whiteSpace: "pre", padding: 10, display: "block" }}>
+          {getTagResult()}
+          <div className='text-end'>
+            <button
+              style={{ marginTop: 10 }}
+              type="button"
+              className="btn btn-light"
+              onClick={openResultWindow}
+            >
+              SHOW NEW WINDOW
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
